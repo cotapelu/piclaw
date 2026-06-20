8c77c472731e888832e4b7c9cb765ca62c21329a
quangtynu <quangbq@gmail.com>
2026-06-20 07:26:28 +0700

    chore: update custom models and todos for v0.0.7 release

--- src/extensions/providers/models/custom-models.generated.ts
+++ src/extensions/providers/models/custom-models.generated.ts
@@ -1,6 +1,6 @@
 // THIS FILE IS AUTO-GENERATED
 // Source: https://models.dev/api.json
-// Generated: 2026-06-19T20:44:13.644Z
+// Generated: 2026-06-20T00:25:33.276Z
 // DO NOT EDIT MANUALLY - Run 'npm run generate-custom-models' to update
 
 import type { Model } from "@earendil-works/pi-ai";
